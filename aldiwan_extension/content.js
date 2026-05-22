db683ff2c8ca63154fe5ebaea0e532e36bda8b54
monesir <monesir@github.local>
2026-05-22 12:36:37 +0300

    Use full titles for suggested poems (v2.1.0)

--- aldiwan_extension/content.js
+++ aldiwan_extension/content.js
@@ -77,18 +77,18 @@
                                     const container = block.querySelector('.content');
                                     if (container) {
                                         h2.dataset.origHtml = container.innerHTML;
-                                        const p1 = decodeURIComponent('%D8%A3%D8%B1%D8%AB%20%D8%AC%D8%AF%D9%8A%D8%AF%20%D8%A7%D9%84%D8%AD%D8%A8%D9%84');
-                                        const p2 = decodeURIComponent('%D9%84%D9%87%D8%A7%D9%86%20%D8%B9%D9%84%D9%8A%D9%86%D8%A7');
-                                        const p3 = decodeURIComponent('%D9%81%D9%8A%20%D8%A7%D9%84%D8%AE%D8%AF');
-                                        const p4 = decodeURIComponent('%D8%A3%D8%B1%D8%A7%D9%83%20%D8%B9%D8%B5%D9%8A%20%D8%A7%D9%84%D8%AF%D9%85%D8%B9');
-                                        const p5 = decodeURIComponent('%D8%A3%D9%84%D8%A7%20%D9%84%D9%8A%D8%AA%20%D8%B4%D8%B9%D8%B1%D9%8A');
+                                        const p1 = decodeURIComponent('%D8%A3%D8%B1%D8%AB%20%D8%AC%D8%AF%D9%8A%D8%AF%20%D8%A7%D9%84%D8%AD%D8%A8%D9%84%20%D9%85%D9%86%20%D8%A3%D9%85%20%D9%85%D8%B9%D8%A8%D8%AF');
+                                        const p2 = decodeURIComponent('%D9%84%D9%87%D8%A7%D9%86%20%D8%B9%D9%84%D9%8A%D9%86%D8%A7%20%D8%A3%D9%86%20%D9%86%D9%82%D9%88%D9%84%20%D9%88%D8%AA%D9%81%D8%B9%D9%84%D8%A7');
+                                        const p3 = decodeURIComponent('%D9%81%D9%8A%20%D8%A7%D9%84%D8%AE%D8%AF%20%D8%A3%D9%86%20%D8%B9%D8%B2%D9%85%20%D8%A7%D9%84%D8%AE%D9%84%D9%8A%D8%B7%20%D8%B1%D8%AD%D9%8A%D9%84%D8%A7');
+                                        const p4 = decodeURIComponent('%D8%A3%D8%B1%D8%A7%D9%83%20%D8%B9%D8%B5%D9%8A%20%D8%A7%D9%84%D8%AF%D9%85%D8%B9%20%D8%B4%D9%8A%D9%85%D8%AA%D9%83%20%D8%A7%D9%84%D8%B5%D8%A8%D8%B1');
+                                        const p5 = decodeURIComponent('%D8%A3%D9%84%D8%A7%20%D9%84%D9%8A%D8%AA%20%D8%B4%D8%B9%D8%B1%D9%8A%20%D9%87%D9%84%20%D8%A3%D8%A8%D9%8A%D8%AA%D9%86%20%D9%84%D9%8A%D9%84%D8%A9');
 
                                         container.innerHTML = `
-<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block text-truncate" href="https://www.aldiwan.net/poem24162.html">${p1}</a></div>
-<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block text-truncate" href="https://www.aldiwan.net/poem30373.html">${p2}</a></div>
-<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block text-truncate" href="https://www.aldiwan.net/poem10125.html">${p3}</a></div>
-<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block text-truncate" href="https://www.aldiwan.net/poem25258.html">${p4}</a></div>
-<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block text-truncate" href="https://www.aldiwan.net/poem80856.html">${p5}</a></div>
+<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block" href="https://www.aldiwan.net/poem24162.html" style="white-space: normal;">${p1}</a></div>
+<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block" href="https://www.aldiwan.net/poem30373.html" style="white-space: normal;">${p2}</a></div>
+<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block" href="https://www.aldiwan.net/poem10125.html" style="white-space: normal;">${p3}</a></div>
+<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block" href="https://www.aldiwan.net/poem25258.html" style="white-space: normal;">${p4}</a></div>
+<div class="col-12 col-md px-2 mb-2"><a class="py-1 d-block" href="https://www.aldiwan.net/poem80856.html" style="white-space: normal;">${p5}</a></div>
                                         `;
                                     }
                                 }
